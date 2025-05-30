5424ebb0ebfec7a6fa15236deb9cdecc34c59497
Srisha <srisha15m@gmail.com>
2025-05-30 08:53:04 +0530

    Additional Files

---
+++ 09/IntroStrings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IntroStrings {
+    public static void main(String[] args) {
+        //1. String var_name = "Value"
+        String str = "Srisha";
+        //2.String var_name = new String("Value")
+        String str2 = new String("Srisha Sharma");
+        //STRINGS ARE IMMUTABLE
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();  //next-> prints till space, nextLine -> prints the whol;e sentence            
+        // System.out.println(name);
+
+        String fullName = "Tony Stark";
+        System.out.println(fullName.length());
+
+
+
+    }
+    
+}
